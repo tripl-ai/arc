1b4e429155bcac894ec6db843187ce54067ad4d1
Mike Seddon <email@address.com>
2019-03-26 11:03:34 +1100

    Build process for databricks

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,7 @@ object Dependencies {
   // Versions
   lazy val sparkVersion = "2.4.0"
   lazy val scalaTestVersion = "3.0.1"
+  lazy val includeJars = if (Option(System.getProperty("assemblyTarget")).getOrElse("standalone") == "databricks") "compile" else "provided"
 
   // Testing
   val scalaTest = "org.scalatest" %% "scalatest" % scalaTestVersion % "it,test"
@@ -12,39 +13,39 @@ object Dependencies {
   // Spark
   val sparkCore = "org.apache.spark" %% "spark-core" % sparkVersion % "provided"
   val sparkSql = "org.apache.spark" %% "spark-sql" % sparkVersion % "provided"
-  val sparkHive = "org.apache.spark" %% "spark-hive" % sparkVersion % "provided"
+  val sparkHive = "org.apache.spark" %% "spark-hive" % sparkVersion % "provided" 
   val sparkMl = "org.apache.spark" %% "spark-mllib" % sparkVersion % "provided"
 
   // Spark XML
-  val sparkXML = "com.databricks" %% "spark-xml" % "0.5.0" % "provided"
+  val sparkXML = "com.databricks" %% "spark-xml" % "0.5.0" % includeJars
 
   // Spark AVRO
-  val sparkAvro = "org.apache.spark" %% "spark-avro" % "2.4.0" % "provided"
+  val sparkAvro = "org.apache.spark" %% "spark-avro" % "2.4.0" % includeJars
 
   // Amazon S3
-  val hadoopAWS = "org.apache.hadoop" % "hadoop-aws" % "2.7.7" % "provided"
-  val awsJavaSDK = "com.amazonaws" % "aws-java-sdk" % "1.11.519" % "provided"
+  val hadoopAWS = "org.apache.hadoop" % "hadoop-aws" % "2.7.7" % includeJars
+  val awsJavaSDK = "com.amazonaws" % "aws-java-sdk" % "1.11.519" % includeJars
 
   // Azure Blob
-  val hadoopAzure = "org.apache.hadoop" % "hadoop-azure" % "2.7.3" % "provided"
-  val azureStorage = "com.microsoft.azure" % "azure-storage" % "3.1.0" % "provided"
+  val hadoopAzure = "org.apache.hadoop" % "hadoop-azure" % "2.7.3" % includeJars
+  val azureStorage = "com.microsoft.azure" % "azure-storage" % "3.1.0" % includeJars
 
   // Azure EventHubs
-  val azureEventHub = "com.microsoft.azure" % "azure-eventhubs" % "1.2.0" % "provided"
-  val qpid = "org.apache.qpid" % "proton-j" % "0.29.0" % "provided"
+  val azureEventHub = "com.microsoft.azure" % "azure-eventhubs" % "1.2.0" % includeJars
+  val qpid = "org.apache.qpid" % "proton-j" % "0.29.0" % includeJars
 
   // Azure AD
-  val azureAD = "com.microsoft.azure" % "adal4j" % "1.2.0" % "provided"
-  val azureKeyVault = "com.microsoft.azure" % "azure-keyvault" % "1.0.0" % "provided"
+  val azureAD = "com.microsoft.azure" % "adal4j" % "1.2.0" % includeJars
+  val azureKeyVault = "com.microsoft.azure" % "azure-keyvault" % "1.0.0" % includeJars
 
   // SQL Server
-  val sqlServerJDBC = "com.microsoft.sqlserver" % "mssql-jdbc" % "7.2.1.jre8" % "it,test,provided"
-  val azureSQLDB = "com.microsoft.azure" % "azure-sqldb-spark" % "1.0.2" % "provided"
-  val postgresJDBC = "org.postgresql" % "postgresql" % "42.2.5" % "it,test,provided"
+  val sqlServerJDBC = "com.microsoft.sqlserver" % "mssql-jdbc" % "7.2.1.jre8" % includeJars
+  val azureSQLDB = "com.microsoft.azure" % "azure-sqldb-spark" % "1.0.2" % includeJars
+  val postgresJDBC = "org.postgresql" % "postgresql" % "42.2.5" % includeJars
 
   // geospark
-  val geospark = "org.datasyslab" % "geospark" % "1.2.0"  % "provided"
-  val geosparkSQL = "org.datasyslab" % "geospark-sql_2.3" % "1.2.0" % "provided"
+  val geospark = "org.datasyslab" % "geospark" % "1.2.0"  % includeJars
+  val geosparkSQL = "org.datasyslab" % "geospark-sql_2.3" % "1.2.0" % includeJars
 
   // cli arg parsing
   val scallop = "org.rogach" %% "scallop" % "2.1.1"
@@ -56,7 +57,7 @@ object Dependencies {
   val scala_graph_json = "org.scala-graph" %% "graph-json" % "1.11.0"
 
   // elasticsearch
-  val elasticsearch = "org.elasticsearch" % "elasticsearch-hadoop" % "6.6.1" % "provided"
+  val elasticsearch = "org.elasticsearch" % "elasticsearch-hadoop" % "6.6.1" % includeJars
 
   // Project
   val etlDeps = Seq(
