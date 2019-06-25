52cbd678e0457196bf379e4812676e50721e5e82
Mike Seddon <seddonm1@gmail.com>
2019-06-25 13:10:54 +1000

    DiffTransform

--- src/main/scala/ai/tripl/arc/transform/DiffTransform.scala
+++ src/main/scala/ai/tripl/arc/transform/DiffTransform.scala
@@ -1,75 +1,144 @@
-// package ai.tripl.arc.transform
+package ai.tripl.arc.transform
 
-// import java.lang._
-// import scala.collection.JavaConverters._
+import java.lang._
+import scala.collection.JavaConverters._
 
-// import org.apache.spark.sql._
-// import org.apache.spark.sql.functions._
-// import org.apache.spark.sql.types._
-// import org.apache.spark.storage.StorageLevel
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.storage.StorageLevel
 
-// import ai.tripl.arc.api.API._
-// import ai.tripl.arc.util._
+import com.typesafe.config._
 
-// object DiffTransform {
+import ai.tripl.arc.api._
+import ai.tripl.arc.api.API._
+import ai.tripl.arc.config._
+import ai.tripl.arc.config.Error._
+import ai.tripl.arc.plugins.PipelineStagePlugin
+import ai.tripl.arc.util.CloudUtils
+import ai.tripl.arc.util.DetailException
+import ai.tripl.arc.util.EitherUtils._
+import ai.tripl.arc.util.ExtractUtils
+import ai.tripl.arc.util.MetadataUtils
+import ai.tripl.arc.util.ListenerUtils
+import ai.tripl.arc.util.Utils
 
-//   def transform(transform: DiffTransform)(implicit spark: SparkSession, logger: ai.tripl.arc.util.log.logger.Logger): Option[DataFrame] = {
-//     val startTime = System.currentTimeMillis() 
-//     val stageDetail = new java.util.HashMap[String, Object]()
-//     stageDetail.put("type", transform.getType)
-//     stageDetail.put("name", transform.name)
-//     for (description <- transform.description) {
-//       stageDetail.put("description", description)    
-//     }
-//     stageDetail.put("inputLeftView", transform.inputLeftView)  
-//     stageDetail.put("inputRightView", transform.inputRightView)   
-//     stageDetail.put("persist", Boolean.valueOf(transform.persist))
+class DiffTransform extends PipelineStagePlugin {
 
-//     logger.info()
-//       .field("event", "enter")
-//       .map("stage", stageDetail)      
-//       .log()      
-    
-//     val inputLeftDF = spark.table(transform.inputLeftView)   
-//     val inputRightDF = spark.table(transform.inputRightView)   
+  val version = Utils.getFrameworkVersion
 
-//     // do a full join on a calculated hash of all values in row on each dataset
-//     // trying to calculate the hash value inside the joinWith method produced an inconsistent result
+  def createStage(index: Int, config: com.typesafe.config.Config)(implicit spark: SparkSession, logger: ai.tripl.arc.util.log.logger.Logger, arcContext: ARCContext): Either[List[ai.tripl.arc.config.Error.StageError], PipelineStage] = {
+    import ai.tripl.arc.config.ConfigReader._
+    import ai.tripl.arc.config.ConfigUtils._
+    implicit val c = config
 
-    
-//     val leftHashDF = inputLeftDF.withColumn("_hash", sha2(to_json(struct(inputLeftDF.columns.map(col):_*)),512))
-//     val rightHashDF = inputRightDF.withColumn("_hash", sha2(to_json(struct(inputRightDF.columns.map(col):_*)),512))
-//     val transformedDF = leftHashDF.joinWith(rightHashDF, leftHashDF("_hash") === rightHashDF("_hash"), "full")
+    val expectedKeys = "type" :: "name" :: "description" :: "environments" :: "inputLeftView" :: "inputRightView" :: "outputIntersectionView" :: "outputLeftView" :: "outputRightView" :: "persist" :: "params" :: Nil
+    val name = getValue[String]("name")
+    val description = getOptionalValue[String]("description")
+    val inputLeftView = getValue[String]("inputLeftView")
+    val inputRightView = getValue[String]("inputRightView")
+    val outputIntersectionView = getOptionalValue[String]("outputIntersectionView")
+    val outputLeftView = getOptionalValue[String]("outputLeftView")
+    val outputRightView = getOptionalValue[String]("outputRightView")
+    val persist = getValue[Boolean]("persist", default = Some(false))
+    val params = readMap("params", c)
+    val invalidKeys = checkValidKeys(c)(expectedKeys)  
 
-//     if (transform.persist && !transformedDF.isStreaming) {
-//       transformedDF.persist(StorageLevel.MEMORY_AND_DISK_SER)
-//     }   
+    (name, description, inputLeftView, inputRightView, outputIntersectionView, outputLeftView, outputRightView, persist, invalidKeys) match {
+      case (Right(name), Right(description), Right(inputLeftView), Right(inputRightView), Right(outputIntersectionView), Right(outputLeftView), Right(outputRightView), Right(persist), Right(invalidKeys)) => 
 
-//     val outputIntersectionDF = transformedDF.filter(col("_1").isNotNull).filter(col("_2").isNotNull).select(col("_1.*")).drop("_hash")
-//     val outputLeftDF = transformedDF.filter(col("_2").isNull).select(col("_1.*")).drop("_hash")
-//     val outputRightDF = transformedDF.filter(col("_1").isNull).select(col("_2.*")).drop("_hash")
+        val stage = DiffTransformStage(
+          plugin=this,
+          name=name,
+          description=description,
+          inputLeftView=inputLeftView,
+          inputRightView=inputRightView,
+          outputIntersectionView=outputIntersectionView,
+          outputLeftView=outputLeftView,
+          outputRightView=outputRightView,
+          params=params,
+          persist=persist
+        )
 
-//     // register views
-//     transform.outputIntersectionView match {
-//       case Some(oiv) => outputIntersectionDF.createOrReplaceTempView(oiv)
-//       case None => 
-//     }
-//     transform.outputLeftView match {
-//       case Some(olv) => outputLeftDF.createOrReplaceTempView(olv)
-//       case None => 
-//     }
-//     transform.outputRightView match {
-//       case Some(orv) => outputRightDF.createOrReplaceTempView(orv)
-//       case None => 
-//     }    
+        stage.stageDetail.put("inputLeftView", inputLeftView)  
+        stage.stageDetail.put("inputRightView", inputRightView)   
+        stage.stageDetail.put("persist", Boolean.valueOf(persist))      
+        for (outputIntersectionView <- outputIntersectionView) {
+          stage.stageDetail.put("outputIntersectionView", outputIntersectionView)  
+        }  
+        for (outputLeftView <- outputLeftView) {
+          stage.stageDetail.put("outputLeftView", outputLeftView)  
+        } 
+        for (outputRightView <- outputRightView) {
+          stage.stageDetail.put("outputRightView", outputRightView)  
+        }                 
 
-//     logger.info()
-//       .field("event", "exit")
-//       .field("duration", System.currentTimeMillis() - startTime)
-//       .map("stage", stageDetail)      
-//       .log()  
+        Right(stage)
+      case _ =>
+        val allErrors: Errors = List(name, description, inputLeftView, inputRightView, outputIntersectionView, outputLeftView, outputRightView, persist, invalidKeys).collect{ case Left(errs) => errs }.flatten
+        val stageName = stringOrDefault(name, "unnamed stage")
+        val err = StageError(index, stageName, c.origin.lineNumber, allErrors)
+        Left(err :: Nil)
+    }
+  }
+}
 
-//     Option(outputIntersectionDF)
-//   }
 
-// }
+case class DiffTransformStage(
+    plugin: PipelineStagePlugin,
+    name: String, 
+    description: Option[String], 
+    inputLeftView: String, 
+    inputRightView: String, 
+    outputIntersectionView: Option[String], 
+    outputLeftView: Option[String], 
+    outputRightView: Option[String], 
+    params: Map[String, String], 
+    persist: Boolean
+  ) extends PipelineStage {
+
+  override def execute()(implicit spark: SparkSession, logger: ai.tripl.arc.util.log.logger.Logger, arcContext: ARCContext): Option[DataFrame] = {
+    DiffTransformStage.execute(this)
+  }
+}
+
+object DiffTransformStage {
+
+  def execute(stage: DiffTransformStage)(implicit spark: SparkSession, logger: ai.tripl.arc.util.log.logger.Logger): Option[DataFrame] = {
+    val stageDetail = stage.stageDetail
+
+    val inputLeftDF = spark.table(stage.inputLeftView)   
+    val inputRightDF = spark.table(stage.inputRightView)   
+
+    // do a full join on a calculated hash of all values in row on each dataset
+    // trying to calculate the hash value inside the joinWith method produced an inconsistent result
+    val leftHashDF = inputLeftDF.withColumn("_hash", sha2(to_json(struct(inputLeftDF.columns.map(col):_*)),512))
+    val rightHashDF = inputRightDF.withColumn("_hash", sha2(to_json(struct(inputRightDF.columns.map(col):_*)),512))
+    val transformedDF = leftHashDF.joinWith(rightHashDF, leftHashDF("_hash") === rightHashDF("_hash"), "full")
+
+    if (stage.persist && !transformedDF.isStreaming) {
+      transformedDF.persist(StorageLevel.MEMORY_AND_DISK_SER)
+    }   
+
+    val outputIntersectionDF = transformedDF.filter(col("_1").isNotNull).filter(col("_2").isNotNull).select(col("_1.*")).drop("_hash")
+    val outputLeftDF = transformedDF.filter(col("_2").isNull).select(col("_1.*")).drop("_hash")
+    val outputRightDF = transformedDF.filter(col("_1").isNull).select(col("_2.*")).drop("_hash")
+
+    // register views
+    stage.outputIntersectionView match {
+      case Some(oiv) => outputIntersectionDF.createOrReplaceTempView(oiv)
+      case None => 
+    }
+    stage.outputLeftView match {
+      case Some(olv) => outputLeftDF.createOrReplaceTempView(olv)
+      case None => 
+    }
+    stage.outputRightView match {
+      case Some(orv) => outputRightDF.createOrReplaceTempView(orv)
+      case None => 
+    }    
+
+    Option(outputIntersectionDF)
+  }
+
+}
--- src/test/scala/ai/tripl/arc/transform/DiffTransformSuite.scala
+++ src/test/scala/ai/tripl/arc/transform/DiffTransformSuite.scala
@@ -1,80 +1,81 @@
-// package ai.tripl.arc
+package ai.tripl.arc
 
-// import java.net.URI
+import java.net.URI
 
-// import org.scalatest.FunSuite
-// import org.scalatest.BeforeAndAfter
+import org.scalatest.FunSuite
+import org.scalatest.BeforeAndAfter
 
-// import org.apache.commons.io.FileUtils
-// import org.apache.commons.io.IOUtils
+import org.apache.commons.io.FileUtils
+import org.apache.commons.io.IOUtils
 
-// import org.apache.spark.sql._
-// import org.apache.spark.sql.functions._
-// import org.apache.spark.sql.types._
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
 
-// import ai.tripl.arc.api._
-// import ai.tripl.arc.api.API._
-// import ai.tripl.arc.util.log.LoggerFactory 
+import ai.tripl.arc.api._
+import ai.tripl.arc.api.API._
+import ai.tripl.arc.util.log.LoggerFactory 
 
-// import ai.tripl.arc.util.TestUtils
+import ai.tripl.arc.util.TestUtils
 
-// class DiffTransformSuite extends FunSuite with BeforeAndAfter {
+class DiffTransformSuite extends FunSuite with BeforeAndAfter {
 
-//   var session: SparkSession = _  
-//   val inputLeftView = "inputLeftView"
-//   val inputRightView = "inputRightView"
-//   val outputIntersectionView = "outputIntersectionView"
-//   val outputLeftView = "outputLeftView"
-//   val outputRightView = "outputRightView"
+  var session: SparkSession = _  
+  val inputLeftView = "inputLeftView"
+  val inputRightView = "inputRightView"
+  val outputIntersectionView = "outputIntersectionView"
+  val outputLeftView = "outputLeftView"
+  val outputRightView = "outputRightView"
 
-//   before {
-//     implicit val spark = SparkSession
-//                   .builder()
-//                   .master("local[*]")
-//                   .config("spark.ui.port", "9999")
-//                   .appName("Spark ETL Test")
-//                   .getOrCreate()
-//     spark.sparkContext.setLogLevel("ERROR")
+  before {
+    implicit val spark = SparkSession
+                  .builder()
+                  .master("local[*]")
+                  .config("spark.ui.port", "9999")
+                  .appName("Spark ETL Test")
+                  .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
     
-//     // set for deterministic timezone
-//     spark.conf.set("spark.sql.session.timeZone", "UTC")   
+    // set for deterministic timezone
+    spark.conf.set("spark.sql.session.timeZone", "UTC")   
 
-//     session = spark
-//     import spark.implicits._ 
-//   }
+    session = spark
+    import spark.implicits._ 
+  }
 
-//   after {
-//     session.stop()
-//   }
+  after {
+    session.stop()
+  }
 
-//   def setNullableStateForAllColumns(df: DataFrame, nullable: Boolean) : DataFrame = {
-//     df.sqlContext.createDataFrame(df.rdd, StructType(df.schema.map(_.copy(nullable = nullable))))
-//   }
+  def setNullableStateForAllColumns(df: DataFrame, nullable: Boolean) : DataFrame = {
+    df.sqlContext.createDataFrame(df.rdd, StructType(df.schema.map(_.copy(nullable = nullable))))
+  }
 
-//   test("DiffTransform") {
-//     implicit val spark = session
-//     import spark.implicits._
-//     implicit val logger = LoggerFactory.getLogger(spark.sparkContext.applicationId)
+  test("DiffTransform") {
+    implicit val spark = session
+    import spark.implicits._
+    implicit val logger = LoggerFactory.getLogger(spark.sparkContext.applicationId)
 
-//     TestUtils.getKnownDataset.createOrReplaceTempView(inputLeftView)
-//     TestUtils.getKnownAlteredDataset.createOrReplaceTempView(inputRightView)
+    TestUtils.getKnownDataset.createOrReplaceTempView(inputLeftView)
+    TestUtils.getKnownAlteredDataset.createOrReplaceTempView(inputRightView)
 
-//     val transformed = transform.DiffTransform.transform(
-//       DiffTransform(
-//         name="DiffTransform", 
-//         description=None,
-//         inputLeftView=inputLeftView,
-//         inputRightView=inputRightView,
-//         outputIntersectionView=Option(outputIntersectionView),
-//         outputLeftView=Option(outputLeftView),
-//         outputRightView=Option(outputRightView),
-//         persist=true,
-//         params=Map.empty
-//       )
-//     )
+    transform.DiffTransformStage.execute(
+      transform.DiffTransformStage(
+        plugin=new transform.DiffTransform,
+        name="DiffTransform", 
+        description=None,
+        inputLeftView=inputLeftView,
+        inputRightView=inputRightView,
+        outputIntersectionView=Option(outputIntersectionView),
+        outputLeftView=Option(outputLeftView),
+        outputRightView=Option(outputRightView),
+        persist=true,
+        params=Map.empty
+      )
+    )
 
-//     assert(spark.table(outputIntersectionView).filter($"integerDatum" === 17).count == 1)
-//     assert(spark.table(outputLeftView).filter($"integerDatum" === 34).count == 1)
-//     assert(spark.table(outputRightView).filter($"integerDatum" === 35).count == 1)
-//   }  
-// }
+    assert(spark.table(outputIntersectionView).filter($"integerDatum" === 17).count == 1)
+    assert(spark.table(outputLeftView).filter($"integerDatum" === 34).count == 1)
+    assert(spark.table(outputRightView).filter($"integerDatum" === 35).count == 1)
+  }  
+}
