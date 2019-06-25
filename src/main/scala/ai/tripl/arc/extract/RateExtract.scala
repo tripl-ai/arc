8032356bf90867fe1f6c6d6fdbe4546087c44186
Mike Seddon <seddonm1@gmail.com>
2019-06-25 10:16:40 +1000

    RateExtract

--- src/main/scala/ai/tripl/arc/extract/RateExtract.scala
+++ src/main/scala/ai/tripl/arc/extract/RateExtract.scala
@@ -1,63 +1,117 @@
-// package ai.tripl.arc.extract
+package ai.tripl.arc.extract
 
-// import java.lang._
-// import java.net.URI
-// import java.util.Properties
-// import scala.collection.JavaConverters._
+import java.lang._
+import java.net.URI
+import java.util.Properties
+import scala.collection.JavaConverters._
 
-// import org.apache.spark.sql._
-// import org.apache.spark.sql.functions._
-// import org.apache.spark.sql.types._
-// import org.apache.spark.storage.StorageLevel
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types._
+import org.apache.spark.storage.StorageLevel
 
-// import ai.tripl.arc.api._
-// import ai.tripl.arc.api.API._
-// import ai.tripl.arc.util._
+import com.typesafe.config._
 
-// object RateExtract {
+import ai.tripl.arc.api._
+import ai.tripl.arc.api.API._
+import ai.tripl.arc.config._
+import ai.tripl.arc.config.Error._
+import ai.tripl.arc.plugins.PipelineStagePlugin
+import ai.tripl.arc.util.CloudUtils
+import ai.tripl.arc.util.DetailException
+import ai.tripl.arc.util.EitherUtils._
+import ai.tripl.arc.util.ExtractUtils
+import ai.tripl.arc.util.MetadataUtils
+import ai.tripl.arc.util.Utils
 
-//   def extract(extract: RateExtract)(implicit spark: SparkSession, logger: ai.tripl.arc.util.log.logger.Logger, arcContext: ARCContext): Option[DataFrame] = {
-//     import spark.implicits._
-//     val startTime = System.currentTimeMillis() 
-//     val stageDetail = new java.util.HashMap[String, Object]()
-//     stageDetail.put("type", extract.getType)
-//     stageDetail.put("name", extract.name)
-//     for (description <- extract.description) {
-//       stageDetail.put("description", description)    
-//     }  
-//     stageDetail.put("outputView", extract.outputView)  
-//     stageDetail.put("rowsPerSecond", Integer.valueOf(extract.rowsPerSecond))
-//     stageDetail.put("rampUpTime", Integer.valueOf(extract.rampUpTime))
-//     stageDetail.put("numPartitions", Integer.valueOf(extract.numPartitions))
+class RateExtract extends PipelineStagePlugin {
 
-//     logger.info()
-//       .field("event", "enter")
-//       .map("stage", stageDetail)      
-//       .log()    
+  val version = Utils.getFrameworkVersion
 
-//     if (!arcContext.isStreaming) {
-//       throw new Exception("RateExtract can only be executed in streaming mode.") with DetailException {
-//         override val detail = stageDetail          
-//       }
-//     }
+  def createStage(index: Int, config: com.typesafe.config.Config)(implicit spark: SparkSession, logger: ai.tripl.arc.util.log.logger.Logger, arcContext: ARCContext): Either[List[ai.tripl.arc.config.Error.StageError], PipelineStage] = {
+    import ai.tripl.arc.config.ConfigReader._
+    import ai.tripl.arc.config.ConfigUtils._
+    implicit val c = config
 
-//     val df = spark.readStream
-//       .format("rate")
-//       .option("rowsPerSecond", extract.rowsPerSecond.toString)
-//       .option("rampUpTime", s"${extract.rampUpTime}s")
-//       .option("numPartitions", extract.numPartitions.toString)
-//       .load
+    val expectedKeys = "type" :: "name" :: "description" :: "environments" :: "outputView" :: "rowsPerSecond" :: "rampUpTime" :: "numPartitions" :: "params" :: Nil
+    val name = getValue[String]("name")
+    val description = getOptionalValue[String]("description")
+    val outputView = getValue[String]("outputView")
+    val rowsPerSecond = getValue[Int]("rowsPerSecond", default = Some(1))
+    val rampUpTime = getValue[Int]("rampUpTime", default = Some(1))
+    val numPartitions = getValue[Int]("numPartitions", default = Some(spark.sparkContext.defaultParallelism))
+    val params = readMap("params", c)
+    val invalidKeys = checkValidKeys(c)(expectedKeys)
 
-//     df.createOrReplaceTempView(extract.outputView)
+    (name, description, outputView, rowsPerSecond, rampUpTime, numPartitions, invalidKeys) match {
+      case (Right(name), Right(description), Right(outputView), Right(rowsPerSecond), Right(rampUpTime), Right(numPartitions), Right(invalidKeys)) => 
 
-//     logger.info()
-//       .field("event", "exit")
-//       .field("duration", System.currentTimeMillis() - startTime)
-//       .map("stage", stageDetail)      
-//       .log()   
+        val stage = RateExtractStage(
+          plugin=this,
+          name=name,
+          description=description,
+          outputView=outputView, 
+          rowsPerSecond=rowsPerSecond,
+          rampUpTime=rampUpTime,
+          numPartitions=numPartitions,
+          params=params
+        )
 
-//     Option(df)
-//   }
+        stage.stageDetail.put("outputView", outputView)  
+        stage.stageDetail.put("rowsPerSecond", Integer.valueOf(rowsPerSecond))
+        stage.stageDetail.put("rampUpTime", Integer.valueOf(rampUpTime))
+        stage.stageDetail.put("numPartitions", Integer.valueOf(numPartitions))
 
-// }
+        Right(stage)
+      case _ =>
+        val allErrors: Errors = List(name, description, outputView, rowsPerSecond, rampUpTime, numPartitions, invalidKeys).collect{ case Left(errs) => errs }.flatten
+        val stageName = stringOrDefault(name, "unnamed stage")
+        val err = StageError(index, stageName, c.origin.lineNumber, allErrors)
+        Left(err :: Nil)
+    }
+  }
+
+}
+
+case class RateExtractStage(
+    plugin: PipelineStagePlugin,
+    name: String,
+    description: Option[String],
+    outputView: String,
+    params: Map[String, String],
+    rowsPerSecond: Int,
+    rampUpTime: Int,
+    numPartitions: Int
+  ) extends PipelineStage {
+
+  override def execute()(implicit spark: SparkSession, logger: ai.tripl.arc.util.log.logger.Logger, arcContext: ARCContext): Option[DataFrame] = {
+    RateExtractStage.execute(this)
+  }
+}
+
+object RateExtractStage {
+
+  def execute(stage: RateExtractStage)(implicit spark: SparkSession, logger: ai.tripl.arc.util.log.logger.Logger, arcContext: ARCContext): Option[DataFrame] = {
+    import spark.implicits._
+    val stageDetail = stage.stageDetail
+
+    if (!arcContext.isStreaming) {
+      throw new Exception("RateExtract can only be executed in streaming mode.") with DetailException {
+        override val detail = stageDetail          
+      }
+    }
+
+    val df = spark.readStream
+      .format("rate")
+      .option("rowsPerSecond", stage.rowsPerSecond.toString)
+      .option("rampUpTime", s"${stage.rampUpTime}s")
+      .option("numPartitions", stage.numPartitions.toString)
+      .load
+
+    df.createOrReplaceTempView(stage.outputView)
+
+    Option(df)
+  }
+
+}
 
