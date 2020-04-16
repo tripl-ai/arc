77611a7d582dcf28de9a26bd88f4f9b5a1346ee0
John Bruce <john.a.m.bruce@gmail.com>
2020-04-17 00:51:06 +1000

    Removed rightFlatMap
    No longer needed for Scala 2.12 and greater

--- src/main/scala/ai/tripl/arc/config/ArcPipeline.scala
+++ src/main/scala/ai/tripl/arc/config/ArcPipeline.scala
@@ -42,7 +42,7 @@ object ArcPipeline {
       case Right(uri) => getConfigString(uri)(spark, logger, arcContext)
     }
 
-    etlConfString.rightFlatMap { etlConfRaw =>
+    etlConfString.flatMap { etlConfRaw =>
       // convert from ipynb to config if required
       val (uriString, configString) = uri match {
         case Right(uri) => {
