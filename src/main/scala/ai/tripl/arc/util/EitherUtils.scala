d559b0090ffe5119b3b29fd53ee8de0bbb9ec680
John Bruce <john.a.m.bruce@gmail.com>
2020-04-17 00:51:06 +1000

    Removed rightFlatMap
    No longer needed for Scala 2.12 and greater

--- src/main/scala/ai/tripl/arc/util/EitherUtils.scala
+++ src/main/scala/ai/tripl/arc/util/EitherUtils.scala
@@ -1,16 +1,10 @@
 package ai.tripl.arc.util
 import scala.language.implicitConversions
 
-// Only needed for Scala 2.11.x can remove when supporting only 2.12.x and greater
 object EitherUtils {
 
     class MappableEither[L, R1](e: Either[L, R1]) {
-
-        def rightFlatMap[R2](mapper: R1 => Either[L, R2]): Either[L, R2] = {
-            e.fold( Left(_) , mapper(_) )
-        }
-
-        def |>[R2](mapper: R1 => Either[L, R2]): Either[L, R2] = rightFlatMap(mapper)
+        def |>[R2](mapper: R1 => Either[L, R2]): Either[L, R2] = e.flatMap(mapper(_)) 
     }
 
     implicit def eitherToMappableEither[L, R](e: Either[L, R]) = new MappableEither(e)
