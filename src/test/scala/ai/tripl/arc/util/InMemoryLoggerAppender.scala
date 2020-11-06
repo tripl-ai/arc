e8997a602f6bb17055b8605baa715dbd3aca73cf
Mike Seddon <seddonm1@gmail.com>
2020-11-07 08:53:00 +1100

    refactor in memory appender

--- src/test/scala/ai/tripl/arc/util/InMemoryLoggerAppender.scala
+++ src/test/scala/ai/tripl/arc/util/InMemoryLoggerAppender.scala
@@ -1,21 +1,19 @@
 package ai.tripl.arc.util
 
-import scala.collection.mutable.StringBuilder
-
 import org.apache.log4j.AppenderSkeleton
 import org.apache.log4j.spi.LoggingEvent
 
 class InMemoryLoggerAppender extends AppenderSkeleton {
 
-  val builder = new StringBuilder()
+  var log = new scala.collection.mutable.ListBuffer[String]()
 
-  def getResult(): String = {
-    builder.toString
+  def getResult(): List[String] = {
+    log.toList
   }
 
   @Override
   def append(event: LoggingEvent) {
-    builder.append(s"${event.getMessage.toString}\n")
+    log += event.getMessage.toString
   }
 
   @Override
